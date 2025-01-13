f3e20379567f224e0123256cbaf38c75cbf13370
BaoKing <huynhbaoking@gmail.com>
2025-01-13 20:25:49 +0700

    fix-module

--- 05-write-your-first-library-package/printer/cmd/main.go
+++ 05-write-your-first-library-package/printer/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "https://github.com/huynhbaoking112/Go_the_final_battle/tree/0090531ea35cd0f2a82a1b67548cff8f579e6a7f/05-write-your-first-library-package/printer"
+import "github.com/inancgumus/learngo/05-write-your-first-library-package/printer"
 
 func main() {
-	printer.H
+	printer.Hello()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module myproject
+module github.com/huynhbaoking112/Go_the_final_battle
 
 go 1.23.4
+
+require github.com/inancgumus/learngo v0.0.0-20240218210645-0bcbf36df396 // indirect
