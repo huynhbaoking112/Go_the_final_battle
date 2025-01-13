78a863b4523a306f1ba9ad5fa6fa9cbfd5fe3330
BaoKing <huynhbaoking@gmail.com>
2025-01-13 21:12:21 +0700

    bai5

---
+++ 05-write-your-first-library-package/exercise/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	goversion "github.com/huynhbaoking112/Go_the_final_battle/05-write-your-first-library-package/exercise"
+)
+
+func main() {
+	fmt.Println(goversion.Get_Go_Version())
+}
